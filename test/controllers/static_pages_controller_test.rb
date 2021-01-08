a3cafab5a440b4b2eeb2ea22436283c1dbfc0623
jmackellar <jessemackellar@gmail.com>
2021-01-08 21:06:56 +0000

    Root route test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @baseTitle = 'Jesse MacKellar'
   end
   
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
